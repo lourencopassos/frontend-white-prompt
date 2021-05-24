d2de01e08ce95b704c8b75647c59e507579f1a7e
Lourenco <lo.passos93@gmail.com>
2021-05-23 22:00:29 -0300

    chore: remove unused code

--- survey-app/src/App.tsx
+++ survey-app/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { AppContext } from "./context/survey/context";
-import { Status, SurveyStep } from "./context/survey/types";
+import { SurveyStep } from "./context/survey/types";
 import StepOne from "./components/Survey-Pop-Up/step-one.index";
 import StepTwo from "./components/Survey-Pop-Up/step-two-index";
 import StepThree from "./components/Survey-Pop-Up/step-three-index";
@@ -12,7 +12,6 @@ function App() {
   const { state, dispatch } = React.useContext(AppContext);
 
   const surveyStep = state.step;
-  const surveyStatus = state.status;
   const surveyActive = state.active;
 
   useEffect(() => {
